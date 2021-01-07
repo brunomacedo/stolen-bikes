5a6c2025890cfbc6b676161f3000b7d4b1b36e44
brunomacedo <contato@brunomacedo.com.br>
2021-01-07 13:48:48 -0300

    fix(server): express build folder

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const secure = require('ssl-express-www');
-const compression = require('compression');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(secure);
-app.use(compression());
-app.use(express.static(`${__dirname}/build`));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
