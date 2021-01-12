9429118025716f8bbbf33bb8bda4c1f7196d62d8
brunomacedo <contato@brunomacedo.com.br>
2021-01-12 02:24:27 -0300

    feat: add pagination on home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,25 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import axios from 'axios';
+import Pagination from 'react-js-pagination';
 import { resquestAllIncidents } from '../../actions';
 import AppContext from '../../context/App';
 import MasterPage from '../../components/MasterPage';
 import SearchForm from '../../components/SearchForm';
 import ResultList from '../../components/ResultList';
 
+import './styled.scss';
+
 export default function Home() {
   const { CancelToken } = axios;
   const source = CancelToken.source();
 
+  const [currentPage, setCurrentPage] = useState(1);
   const { allIncidents, updateIncidents } = useContext(AppContext);
 
-  const getAllIncidents = async () => {
+  const getAllIncidents = async (page: number) => {
     try {
       const { incidents } = await resquestAllIncidents({
-        page: 1,
+        page,
         per_page: 10,
         incident_type: 'theft',
         occurred_before: '',
@@ -31,8 +35,13 @@ export default function Home() {
     }
   };
 
+  const onHandlePageChange = async (page: number) => {
+    setCurrentPage(page);
+    await getAllIncidents(page);
+  };
+
   useEffect(() => {
-    getAllIncidents();
+    getAllIncidents(currentPage);
 
     return () => {
       source.cancel('Operation canceled by the user.');
@@ -43,6 +52,22 @@ export default function Home() {
     <MasterPage>
       <SearchForm />
       <ResultList list={allIncidents} />
+
+      {allIncidents.length > 0 && (
+        <Pagination
+          firstPageText="<< First"
+          prevPageText="< Prev"
+          nextPageText="Next >"
+          lastPageText="Last >>"
+          innerClass="list-pagination"
+          linkClass="list-pagination--link"
+          activeLinkClass="list-pagination--active"
+          activePage={currentPage}
+          totalItemsCount={100}
+          pageRangeDisplayed={3}
+          onChange={(page: number) => onHandlePageChange(page)}
+        />
+      )}
     </MasterPage>
   );
 }
